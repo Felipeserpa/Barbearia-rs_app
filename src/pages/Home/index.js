78dc80670e68f1064ab22d433aafce171227294b
Felipe serpa <felipe.mago_skt@hotmail.com>
2024-10-23 09:59:26 -0300

    atualizacao pagina de home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,24 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
 
-export default function Home() {
+export default function Home({ navigation }) {
   return (
-    <View>
+    <View style={style.container}>
       <Text>This is some text</Text>
+      <Button
+        title="Go to Jane's profile"
+        onPress={() => navigation.navigate("Login", { name: "Jane" })}
+      />
     </View>
   );
 }
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#172554",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: 0,
+  },
+});
