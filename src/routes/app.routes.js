01fc9e6639c0a0d8de4d653b2211d8764e8f97ef
Felipe serpa <felipe.mago_skt@hotmail.com>
2024-11-06 10:05:30 -0300

    implementaçao da pagina servicos

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -5,6 +5,7 @@ import { createDrawerNavigator } from "@react-navigation/drawer";
 import Home from "../pages/HomeScreen";
 import Sobre from "../pages/Sobre";
 import Perfil from "../pages/Perfil";
+import Servicos from "../pages/Serviços";
 const AppDrawer = createDrawerNavigator();
 
 export default function AppRoutes() {
@@ -12,6 +13,7 @@ export default function AppRoutes() {
     <AppDrawer.Navigator>
       <AppDrawer.Screen name="Agenda" component={Home} />
       <AppDrawer.Screen name="Perfil" component={Perfil} />
+      <AppDrawer.Screen name="Serviços" component={Servicos} />
       <AppDrawer.Screen name="Sobre" component={Sobre} />
     </AppDrawer.Navigator>
   );
