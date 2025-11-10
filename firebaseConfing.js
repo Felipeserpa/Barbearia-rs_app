a088baf3d4446fbeb4afca04ba9204e250712045
Felipe serpa <felipe.mago_skt@hotmail.com>
2025-11-10 09:22:46 -0300

    implementaçao do firebase

--- firebaseConfing.js
+++ firebaseConfing.js
@@ -1,12 +1,3 @@
-
-Usar o npm
-
-Usar a tag <script>
-Se você já estiver usando o npm e um bundler de módulos, como webpack ou Rollup, execute o seguinte comando para instalar o SDK mais recente (Saiba mais):
-
-npm install firebase
-Depois inicialize o Firebase e comece a usar os SDKs dos produtos.
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -19,7 +10,7 @@ const firebaseConfig = {
   projectId: "barbechatbot",
   storageBucket: "barbechatbot.firebasestorage.app",
   messagingSenderId: "51035787161",
-  appId: "1:51035787161:web:b9bdb77ac521c6df3c8d08"
+  appId: "1:51035787161:web:b9bdb77ac521c6df3c8d08",
 };
 
 // Initialize Firebase
